339d5765356001475ec10452d1ec735c53a38845
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-12-08 00:00:59 +0530

    added navbar

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 function Home() {
   return (
     <>
+    <Navbar/> 
     <h1 className='text-center'>HomePage</h1>
     </>
   )
--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 function Signup() {
   return (
-    <div>Signup</div>
+   <>
+   <Navbar/>
+   </>
   )
 }
 
