121d6601d626dfa51b38a45092565e739d3d5264
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-12-07 22:47:02 +0530

    feat: added jwt token in signup or login api

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,48 +1,97 @@
 import User from "../model/User.js";
+import jwt from "jsonwebtoken";
 
 const postApiSignup = async (req, res) => {
-  const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-  const signupObj = new User({
-    username,
-    email,
-    password,
-  });
-  const savedUser = await signupObj.save();
+    if (!email || !password || !username) {
+        return res.json({
+          success: false,
+          message: "Please enter all fields",
+        });
+      }
+    const signupObj = new User({
+      username,
+      email,
+      password,
+    });
+    const savedUser = await signupObj.save();
 
-  res.json({
-    success: true,
-    data: savedUser,
-    message: "user saved successfully...",
-  });
+    if (!process.env.SECRET_KEYS) {
+      return res.status(500).json({
+        success: false,
+        message: "Secret key not defined",
+      });
+    }
+
+    const token = jwt.sign(
+      { userId: savedUser._id, isAdmin: true },
+      process.env.SECRET_KEYS,
+      { expiresIn: "1h" } 
+    );
+
+    res.json({
+      success: true,
+      data: {
+        savedUser: savedUser,
+        jwt_token: token,
+      },
+      message: "User saved successfully...",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 };
 
 const postApiLogin = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.json({
-      success: false,
-      message: "please provide email and password",
-    });
-  }
-  const user = await User.findOne({
-    email: email,
-    password: password,
-  }).select("username email");
+    if (!email || !password) {
+      return res.json({
+        success: false,
+        message: "Please provide email and password",
+      });
+    }
 
-  if (user) {
-    return res.json({
-      success: true,
-      data: user,
-      message: "login successfull...",
-    });
-  } else {
-    return res.json({
-      success: false,
-      message: "invalid credentials...",
-    });
+    const user = await User.findOne({ email, password }).select("username email");
+
+    if (user) {
+      if (!process.env.SECRET_KEYS) {
+        return res.status(500).json({
+          success: false,
+          message: "Secret key not defined",
+        });
+      }
+      const token = jwt.sign(
+        { userId: user._id, isAdmin: true },
+        process.env.SECRET_KEYS,
+        { expiresIn: "1h" } 
+      );
+
+      return res.json({
+        success: true,
+        data: {
+          user: user,
+          email: email,
+          jwt_token: token,
+        },
+        message: "Login successful...",
+      });
+    } else {
+      return res.json({
+        success: false,
+        message: "Invalid credentials...",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 
 export { postApiSignup, postApiLogin };
+
+
